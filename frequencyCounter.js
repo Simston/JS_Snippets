8a2d349d2beaf81d37deceec3522bf0dee6631aa
Simston <stephane.simon51@gmail.com>
2023-08-04 19:10:40 +0200

    Another elegant solution

--- frequencyCounter.js
+++ frequencyCounter.js
@@ -1,6 +1,7 @@
 // Given two strings, write a function to determine if the second string is an anagram of the first. 
 // An anagram is a word, phrase, or name formed by rearranging the letters of another, such as cinema, formed from iceman.
 
+// MY SOLUTION
 function validAnagram(str1, str2) {
     if (str1.length !== str2.length) {
         return false;
@@ -14,7 +15,7 @@ function validAnagram(str1, str2) {
         frequencyCounter2[val] = (frequencyCounter2[val] || 0) + 1;
     }
     console.log(frequencyCounter1);
-    console.log(frequencyCounter2)
+    console.log(frequencyCounter2);
     for (const val in frequencyCounter1) {
         if (!(val in frequencyCounter2)) {
             return false;
@@ -26,5 +27,35 @@ function validAnagram(str1, str2) {
     return true;
 }
 // TIME COMPLEXITY HERE - 0(n)
+//console.log(validAnagram('aaz', 'zza'));
 
-console.log(validAnagram('aaz', 'zza'));
+//ANOTHER SOLUTION
+function validAnagram2(str1, str2) {
+    if (str1.length !== str2.length) {
+        return false;
+    }
+
+    const lookup = {};
+
+    for (let i = 0; i < str1.length; i++) {
+        let letter = str1[i];
+        //if letter exists, increment, otherwise set to 1
+        lookup[letter] ? lookup[letter] += 1 : lookup[letter] = 1;
+    }
+    console.log(lookup)
+
+    for (let i = 0; i < str2.length; i++) {
+        let letter = str2[i];
+        console.log(letter)
+        console.log(lookup[letter])
+        // can"t find letter or letter is zero then it's not an anagram
+        // so if lookup[letter] === false (in this case 0 (false)) 
+        if (!lookup[letter]) {
+            return false;
+        } else {
+            lookup[letter] -= 1;
+        }
+    }
+    return true;
+}
+console.log(validAnagram2('anagrams', 'nagaramm'));
