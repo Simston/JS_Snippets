887a9f6467407c7f6665c4b6d3c071e6b2ecd340
Simston <stephane.simon51@gmail.com>
2023-08-04 17:38:13 +0200

    Another soluce

--- same.js
+++ same.js
@@ -78,3 +78,20 @@ function same(array1, array2) {
 //same([1, 2, 1], [4, 4, 1]);
 
 // Another Solution 
+function same2(arr1, arr2) {
+    if (arr1.length !== arr2.length) {
+        return false;
+    }
+    for (let i = 0; i < arr1.length; i++) {
+        // ** for square²
+        let correctIndex = arr2.indexOf(arr1[i] ** 2);
+        if (correctIndex === -1) {
+            return false;
+        }
+        arr2.splice(correctIndex, 1);
+    }
+    return true;
+}
+// TIME COMPLEXITY HERE - N²
+console.log(same2([1, 2, 3], [4, 1, 9]));
+console.log(same2([1, 2, 1], [4, 4, 1]));
