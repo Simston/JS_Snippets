ada3687f784c2563b4d1022939d5f98ec2f36e90
Simston <stephane.simon51@gmail.com>
2023-08-04 18:36:24 +0200

    Another same in 0(n)

--- same.js
+++ same.js
@@ -92,6 +92,33 @@ function same2(arr1, arr2) {
     }
     return true;
 }
-// TIME COMPLEXITY HERE - N²
-console.log(same2([1, 2, 3], [4, 1, 9]));
-console.log(same2([1, 2, 1], [4, 4, 1]));
+// TIME COMPLEXITY HERE - 0(n²)
+//console.log(same2([1, 2, 3], [4, 1, 9]));
+//console.log(same2([1, 2, 1], [4, 4, 1]));
+
+//ANOTHER SOLUTION
+function same3(arr1, arr2) {
+    if (arr1.length !== arr2.length) {
+        return false;
+    }
+    let frequencyCounter1 = {};
+    let frequencyCounter2 = {};
+    for (const val of arr1) {
+        frequencyCounter1[val] = (frequencyCounter1[val] || 0) + 1;
+    }
+    for (const val of arr2) {
+        frequencyCounter2[val] = (frequencyCounter2[val] || 0) + 1;
+    }
+    for (const key in frequencyCounter1) {
+        if (!(key ** 2 in frequencyCounter2)) {
+            return false;
+        }
+        if (frequencyCounter2[key ** 2] !== frequencyCounter1[key]) {
+            return false;
+        }
+    }
+    return true;
+}
+// TIME COMPLEXITY HERE - 0(n)
+
+console.log(same3([1, 2, 3], [4, 1, 9]));
