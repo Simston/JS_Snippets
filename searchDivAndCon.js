5b5f071d0e291037e230ef9d5b61743eb84c66c8
Simston <stephane.simon51@gmail.com>
2023-08-06 14:54:47 +0200

    search divide and conquer

---
+++ searchDivAndCon.js
@@ -0,0 +1,23 @@
+// Divide and Conquer with a search function
+function search(arr, val) {
+
+    let min = 0;
+    let max = arr.length - 1;
+
+    while (min <= max) {
+        let middle = Math.floor((min + max) / 2);
+        let currentElement = arr[middle];
+
+        if (currentElement < val) {
+            min = middle + 1;
+        }
+        else if (currentElement > val) {
+            max = middle - 1;
+        }
+        else {
+            return middle;
+        }
+    }
+    return -1;
+}
+console.log(search([1, 3, 4, 6, 8], 6));
