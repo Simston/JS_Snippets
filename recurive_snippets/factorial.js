e1feaef59742e5456028e5487da49713ac769adb
Simston <stephane.simon51@gmail.com>
2023-08-07 15:28:45 +0200

    Update

---
+++ recurive_snippets/factorial.js
@@ -0,0 +1,16 @@
+// Iterative Solution
+function factorial(num) {
+    let total = 1;
+    for (let i = num; i > 0; i--) {
+        total *= i;
+    }
+    return total;
+}
+
+// Recursive Solution
+function recursiveFactorial(num) {
+    if (num === 1) return 1;
+    let total = num * recursiveFactorial(num - 1);
+    return total;
+}
+console.log(recursiveFactorial(4))
