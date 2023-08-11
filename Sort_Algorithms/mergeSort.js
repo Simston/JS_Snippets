982d3bea2e84b39817ca93b94b0e63f611544808
Simston <stephane.simon51@gmail.com>
2023-08-11 16:02:05 +0200

    Clean code

--- Sort_Algorithms/mergeSort.js
+++ Sort_Algorithms/mergeSort.js
@@ -4,13 +4,9 @@ function mergeSort(array) {
     let mid = Math.floor(array.length / 2);
     let array1 = mergeSort(array.slice(0, mid));
     let array2 = mergeSort(array.slice(mid));
-
     return mergeArrays(array1, array2)
 }
-
 console.log(mergeSort([1, 2, 4, 4, 5, 6, 11, 8, 9, 10]));
-
-
 
 function mergeArrays(arr1, arr2) {
     let mergeArray = [];
@@ -26,13 +22,11 @@ function mergeArrays(arr1, arr2) {
             j++;
         }
     }
-
     // Add the remaining elements from arr1 to mergeArray.
     while (i < arr1.length) {
         mergeArray.push(arr1[i]);
         i++;
     }
-
     // Add the remaining elements from arr2 to mergeArray.
     while (j < arr2.length) {
         mergeArray.push(arr2[j]);
