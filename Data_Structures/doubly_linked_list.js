16dfe4446f276156844bb6fae3dd69ab8c777d3f
Simston <32360996+Simston@users.noreply.github.com>
2023-08-24 22:46:43 +0200

    future methods

--- Data_Structures/doubly_linked_list.js
+++ Data_Structures/doubly_linked_list.js
@@ -25,12 +25,33 @@ class DoublyLinkedList {
         this.length++;
         return this;
     }
+    pop() {
+        if (!this.head) return undefined;
+        let tmpReturn = this.tail;
+        if (this.length === 1) {
+            this.head = null;
+            this.tail = null;
+        }
+        let newTail = tmpReturn.prev;
+        this.tail = newTail;
+        newTail.next = null;
+        this.length--;
+        return tmpReturn;
+    }
+    shift() {
+
+    }
+    unshift() {
+
+    }
 }
 
 let list = new DoublyLinkedList();
 list.push(3);
-list.push(4)
-list.push(99)
+list.push(4);
+list.push(44);
+list.push(55);
+list.push(99);
 
-
+list.pop()
 console.log(list);
